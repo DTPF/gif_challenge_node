86f88da374e283c0f886ce0445831e21e834eddb
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-23 00:03:03 +0200

    feat(makeResponse): create function for easy responses

---
+++ src/utils/makeResponse.js
@@ -0,0 +1,10 @@
+const makeResponse = (res, code, msg, result, err) => {
+	return res.status(code).send({ status: code, message: msg, result, error: err })
+}
+
+const response500 = (err) => makeResponse(res, 500, 'Server error', err)
+
+module.exports = {
+	makeResponse,
+	response500
+}
