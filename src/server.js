12e94a3788a45a3554d5b690da49afa13cb25cf2
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-22 20:57:34 +0200

    change(server): add user, gif, and category router

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ const app = express()
 const errorMiddleware = require('./middlewares/error.middleware')
 const fileUpload = require('express-fileupload')
 // Routes
+const userRouter = require('./router/user.router')
+const gifRouter = require('./router/gif.router')
+const categoryRouter = require('./router/category.router')
 
 app.use(express.json())
 app.use(cors({
@@ -22,6 +25,9 @@ app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "cross-origin")
   next()
 })
+app.use(`/api/${config.app.API_VERSION}`, userRouter)
+app.use(`/api/${config.app.API_VERSION}`, gifRouter)
+app.use(`/api/${config.app.API_VERSION}`, categoryRouter)
 app.use(errorMiddleware)
 
 module.exports = app;
