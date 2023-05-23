83df79c34ac061fbb5dcee601af27d0f10c85690
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-23 21:14:47 +0200

    change(gifModel): add categories and external url

--- src/models/gif.model.js
+++ src/models/gif.model.js
@@ -3,8 +3,13 @@ const { Schema, model } = require('mongoose')
 const GifSchema = new Schema({
   name: { type: String, required: true },
   imageUrl: String,
+  externalImageUrl: String,
   imagePublicId: String,
   timesShared: Number,
+  categories: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Category'
+  }],
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User'
