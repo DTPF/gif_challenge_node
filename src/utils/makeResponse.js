e1551981bbf4d834b0bcc270d98a2469a79441fc
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-23 21:12:34 +0200

    fix(makeResponse): missing parameter

--- src/utils/makeResponse.js
+++ src/utils/makeResponse.js
@@ -2,7 +2,7 @@ const makeResponse = (res, code, msg, result, err) => {
 	return res.status(code).send({ status: code, message: msg, result, error: err })
 }
 
-const response500 = (err) => makeResponse(res, 500, 'Server error', err)
+const response500 = (res, err) => makeResponse(res, 500, 'Server error', err)
 
 module.exports = {
 	makeResponse,
