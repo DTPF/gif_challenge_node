6090d34d9364c600a58e61e6b24d971b51db15ff
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-24 17:39:49 +0200

    change(categories): add new categories

--- src/db_seeder/category.seeder.js
+++ src/db_seeder/category.seeder.js
@@ -52,6 +52,48 @@ function getBaseCategory() {
     },
     {
       _id: new ObjectId(),
+      name: 'Annoyed',
+      imageUrl: 'https://res.cloudinary.com/da0fcwrip/image/upload/v1684940380/development/categoryImages/zkzmtncs7k3zyhwn6yn9.gif',
+      imagePublicId: 'development/categoryImages/zkzmtncs7k3zyhwn6yn9'
+    },
+    {
+      _id: new ObjectId(),
+      name: 'Smile',
+      imageUrl: 'https://res.cloudinary.com/da0fcwrip/image/upload/v1684940484/development/categoryImages/qszb9d5flzej6aikjw1h.gif',
+      imagePublicId: 'development/categoryImages/qszb9d5flzej6aikjw1h'
+    },
+    {
+      _id: new ObjectId(),
+      name: 'Ew',
+      imageUrl: 'https://res.cloudinary.com/da0fcwrip/image/upload/v1684941244/development/categoryImages/f3iavxhsxgabyj7pkovm.gif',
+      imagePublicId: 'development/categoryImages/f3iavxhsxgabyj7pkovm'
+    },
+    {
+      _id: new ObjectId(),
+      name: 'Surprised',
+      imageUrl: 'https://res.cloudinary.com/da0fcwrip/image/upload/v1684941310/development/categoryImages/khzexwf13ojfiudzp6m1.gif',
+      imagePublicId: 'development/categoryImages/khzexwf13ojfiudzp6m1'
+    },
+    {
+      _id: new ObjectId(),
+      name: 'Oops',
+      imageUrl: 'https://res.cloudinary.com/da0fcwrip/image/upload/v1684941373/development/categoryImages/ocmp61zdtnu4hnzgtf83.gif',
+      imagePublicId: 'development/categoryImages/ocmp61zdtnu4hnzgtf83'
+    },
+    {
+      _id: new ObjectId(),
+      name: 'Clapping',
+      imageUrl: 'https://res.cloudinary.com/da0fcwrip/image/upload/v1684941421/development/categoryImages/k8mb7zw61vomr8xuosqy.gif',
+      imagePublicId: 'development/categoryImages/k8mb7zw61vomr8xuosqy'
+    },
+    {
+      _id: new ObjectId(),
+      name: 'Embarassed',
+      imageUrl: 'https://res.cloudinary.com/da0fcwrip/image/upload/v1684942366/development/categoryImages/vyv9wmwwzaxnzvw7ka8f.gif',
+      imagePublicId: 'development/categoryImages/vyv9wmwwzaxnzvw7ka8f'
+    },
+    {
+      _id: new ObjectId(),
       name: 'Others',
       imageUrl: 'https://res.cloudinary.com/da0fcwrip/image/upload/v1684779433/development/categoryImages/dbqi5c7j8dx6sy3enh1a.gif',
       imagePublicId: 'development/categoryImages/dbqi5c7j8dx6sy3enh1a'
