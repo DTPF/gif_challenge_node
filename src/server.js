30edd71b94a217b63c66662048532d7c88d6b9c9
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-24 20:14:46 +0200

    change(server.js): add helmet

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const config = require('./config/config')
+const helmet = require('helmet')
 const cors = require('cors')
 const app = express()
 const errorMiddleware = require('./middlewares/error.middleware')
@@ -12,6 +13,24 @@ const categoryRouter = require('./router/category.router')
 app.use(express.json())
 app.use(cors({
   origin: ['http://localhost:5170']
+}))
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: [
+        "'self'",
+        "https://dev-qgjlr8hrde6g23pf.us.auth0.com",
+        "https://lh3.googleusercontent.com",
+        "https://asset.cloudinary.com/da0fcwrip/0ccc85f14f7d0aca0ad67fd8d5eafdc4"
+      ],
+      objectSrc: ["'none'"],
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'", "https: 'unsafe-inline'"],
+      connectSrc: ["'self'", "https://dev-qgjlr8hrde6g23pf.us.auth0.com/oauth/token"],
+      "img-src": ["'self'", "https: data:"],
+      upgradeInsecureRequests: [],
+    },
+  },
 }))
 app.use(
   fileUpload({
